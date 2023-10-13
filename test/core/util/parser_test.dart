e2738a5b5fab01219a48b8979b89c8ef77febd29
Tushar Ghige <dev.tushar.g@gmail.com>
2023-10-13 21:15:15 +0530

    Added unit tests for Parser

--- test/core/util/parser_test.dart
+++ test/core/util/parser_test.dart
@@ -1,1 +1,21 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stock_scan_parser/core/util/parser.dart';
+
+void main() {
+  group('resolveColor', () {
+    test(
+        '[Parser.resolveColor] should return [Colors.red] for correct red color input',
+        () {
+      final color = Parser.resolveColor("red");
+      expect(color, Colors.red);
+    });
+
+    test(
+        '[Parser.resolveColor] should return [Colors.green] for correct green color input',
+        () {
+      final color = Parser.resolveColor("green");
+      expect(color, Colors.green);
+    });
+  });
+}
